938b0a467c4d03d77e3aecb41f40ac11fb3f9777
papi <papi-menu@yandex.ru>
2019-01-22 03:03:59 +0600

    asdkalkjsflk

---
+++ src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  state = { term: "" };
+
+  render() { 
+    return ( <div className="ui segment">
+      <form className="ui form">
+        <div className="field">
+        <label>Image search</label>
+          <input type='text' value={this.state.term} onChange={e => this.setState({term: e.target.value})}  />
+        </div>
+      </form>
+    </div> );
+  }
+}
+ 
+export default SearchBar;
